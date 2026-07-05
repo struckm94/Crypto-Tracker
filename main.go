2a8dbc4a31064b770de354de1af99443aea73645
Matt Struck <matthewstruck@gmail.com>
2026-07-04 19:39:31 -0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+/*
+
+CoinGecko API - Keyless: https://docs.coingecko.com/docs/keyless-public-api
+curl -X GET \
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana&vs_currencies=usd"
+
+*/
